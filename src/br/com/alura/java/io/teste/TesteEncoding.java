6364c125e156745821a2a5e6a67321e8777b6bab
daviaguedes <77637069+daviaguedes@users.noreply.github.com>
2021-06-30 17:24:53 -0300

    Estudo Encoding

--- src/br/com/alura/java/io/teste/TesteEncoding.java
+++ src/br/com/alura/java/io/teste/TesteEncoding.java
@@ -1,12 +1,36 @@
 package br.com.alura.java.io.teste;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class TesteEncoding {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
 
 		
-		String s = "C";
+		String s = "ç";
 		System.out.println(s.codePointAt(0));
+		
+		Charset charset = Charset.defaultCharset();
+		System.out.println(charset.displayName());
+		
+		byte[] bytes = s.getBytes("windows-1252");
+		System.out.print(bytes.length + " " + " windows-1252 ");
+		
+		String novoS = new String(bytes, "windows-1252");
+		System.out.println(novoS);
+		
+		bytes = s.getBytes("UTF-16");
+		System.out.print(bytes.length + " " + " UTF-16 " );
+		novoS = new String(bytes, "UTF-16");
+		System.out.println(novoS);
+		
+		bytes = s.getBytes(StandardCharsets.US_ASCII);
+		System.out.print(bytes.length + " " + " US-ASCII " );
+		novoS = new String(bytes, "US-ASCII");
+		System.out.println(novoS);
+		
 	}
 
 }
